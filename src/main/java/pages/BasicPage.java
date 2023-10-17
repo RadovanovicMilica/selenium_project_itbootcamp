9ba37b6917afca177a3429039e23746314579c04
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-17 15:14:57 +0200

    add BasicPage

--- src/main/java/pages/BasicPage.java
+++ src/main/java/pages/BasicPage.java
@@ -1,2 +1,26 @@
-package pages;public class BasicPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasicPage {
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+
+    public BasicPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+    public boolean elementExists(By by) {
+        return !driver.findElements(by).isEmpty();
+    }
+    public static boolean elementExists(By by, int seconds){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
+        boolean elementExists = !driver.findElements(by).isEmpty();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return elementExists;
+    }
 }
