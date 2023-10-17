c3b33f16b67f548167d48f9b9e49f17f38253749
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-17 16:24:29 +0200

    Add MessagePopUp Page

--- src/main/java/pages/MessagePopUpPage.java
+++ src/main/java/pages/MessagePopUpPage.java
@@ -1,2 +1,26 @@
-package pages;public class MessagePopUpPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MessagePopUpPage extends BasicPage{
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+    public WebElement getErrorPopup() {
+        By errorPopupLocator = By.cssSelector(".v-snack__content ul li");
+        return wait
+                .until(ExpectedConditions.visibilityOfElementLocated(errorPopupLocator));
+    }
+    public String getErrorText() {
+        return getErrorPopup().getText();
+    }
+    public void waitUntilErrorPopupIsVisible () {
+        wait
+                .withMessage("Pop-up message for invalid login should be visible.")
+                .until(ExpectedConditions.visibilityOf(getErrorPopup()));
+    }
 }
