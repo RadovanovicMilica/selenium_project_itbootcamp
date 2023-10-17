999ae7af77ca25509fd22a388248788b3171b2c1
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-17 15:17:05 +0200

    Login page commit

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,2 +1,41 @@
-package pages;public class LoginPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//Login Page
+//        Polje za unos email-a
+//        Polje za unos lozinke
+//        Dugme za login.
+
+public class LoginPage extends BasicPage{
+
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public WebElement getEmailInput() {
+        return driver.findElement(By.id("email"));
+    }
+    public void clearAndTypeEmailInput(String email) {
+        getEmailInput().clear();
+        getEmailInput().sendKeys(email);
+    }
+    public WebElement getPasswordInput() {
+        return driver.findElement(By.id("password"));
+    }
+    public void clearAndTypePassword(String password) {
+        getPasswordInput().clear();
+        getPasswordInput().sendKeys(password);
+    }
+    public WebElement getLoginButton() {
+        return driver.findElement(By.className("v-btn--is-elevated"));
+    }
+    public void clickOnLoginButton() {
+        getLoginButton().click();
+    }
+
 }
