a94a2f44e0d46c1b438168a43881e37830a8293b
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-17 15:12:16 +0200

    add only BasicTest

--- src/test/java/tests/BasicTest.java
+++ src/test/java/tests/BasicTest.java
@@ -1,2 +1,53 @@
-package tests;public class BasicTest {
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import pages.LoginPage;
+import pages.MessagePopUpPage;
+import pages.NavPage;
+
+import java.time.Duration;
+
+public class BasicTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl= "https://vue-demo.daniel-avellaneda.com";
+    protected LoginPage loginPage;
+    protected NavPage navPage;
+    protected MessagePopUpPage messagePopUpPage;
+
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        navPage = new NavPage(driver, wait);
+        loginPage = new LoginPage(driver, wait);
+        messagePopUpPage= new MessagePopUpPage(driver, wait);
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.navigate().to(baseUrl);
+    }
+    @AfterMethod
+    public void afterMethod() {
+        driver.manage().deleteAllCookies();
+
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
 }
