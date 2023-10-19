1e381cde6980b7bb3c1aae2709d24f435b984504
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-20 01:32:36 +0200

    Test Scenario: Auth Routes Tests- Tesc Case: Forbids visits to profile url if not authenticated

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -15,4 +15,14 @@ public class AuthRoutesTests extends BasicTest{
                 baseUrl + "/login",
                 "Error! Current URL isn't " + baseUrl+ "/login");
     }
+    @Test (priority = 2, retryAnalyzer = danielRetry.class)
+    public void forbidsVisitsToProfileUrlIfNotAuthenticated (){
+//        Koraci:
+//        Ucitati /profile stranu
+//        Verifikovati da se u url-u stranice javlja ruta /login
+        driver.navigate().to(baseUrl + "/profile");
+        Assert.assertEquals(navPage.getCurrentUrl(),
+                baseUrl + "/login",
+                "Error! Current URL isn't " + baseUrl+ "/login");
+    }
 }
