df0e8dbfb75f58dd4c8085676a1db459fdd7b0a9
RadovanovicMilica <mickebgbd5@gmail.com>
2023-10-20 02:40:26 +0200

    Test Scenario: Locale Tests - Test Case: Set locale ns EN

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -18,5 +18,17 @@ public class LocaleTests extends BasicTest{
                 "Página de aterrizaje",
                 "Error! Text in Header is not 'Página de aterrizaje'.");
     }
+    @Test (priority = 2, retryAnalyzer = danielRetry.class)
+    public void setLocaleToEN (){
+//        Postaviti jezik na EN
+//        Verifikovati da se na stranici u hederu javlja tekst Landing
+        navPage.clickLanguageButton();
+        navPage.waitUntilLanguageListIsVisible();
+        navPage.clickLanguageEN();
+        Assert.assertEquals(navPage.getHeaderText(),
+                "Landing",
+                "Error! Text in Header is not 'Landing'.");
+
+    }
 
 }
